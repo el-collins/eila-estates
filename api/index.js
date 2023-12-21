f8853d0fb0e7ebeda9e72549a06c063cacd39555
El-Collins <charlescollins476@gmail.com>
2023-12-21 19:53:20 +0100

    Create user model

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// 02 connect app to database
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB!");
 }).catch((err)=>{
     console.log(err);
 });
 
-// create server on port 30000
+// 01 create server on port 30000
 const app = express();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
 });
 
-// connect app to database
