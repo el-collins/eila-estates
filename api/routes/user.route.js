3678523a8f2e1367f6e82a558f796b6de55945db
El-Collins <charlescollins476@gmail.com>
2023-12-26 08:47:42 +0100

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {test} from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-// get is used to get requests 
+// get is used to get requests
 // if you want to send to the db you have to use post request
-router.get('/test', test)
+router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
